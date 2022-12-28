468c4e9ac3eec3061f3d8e635f729a314c38bd50
kiran_a <iamkiranashok@gmail.com>
2022-12-28 22:27:59 +0530

    Third commit

--- src/Date.java
+++ src/Date.java
@@ -1,57 +1,10 @@
 
 
 public class Date {
-    int Day,Month,Year;
-    public Date(int Day,int Month,int Year)
-    {
-        this.Day=Day;
-        this.Month=Month;
-        this.Year=Year;
-    }
-    public int getMonth()
-    {
-    return Month;
-    }
-    public int getDay()
-    {
-        return Day;
 
-    }
-    public int getYear()
-    {
-        return Year;
-    }
-
-
-    public void setMonth(int i) {
-
-
-    }
-
-    public void setDay(int i) {
-
-    }
-
-    public int setYear(int i) {
-        return Year;
-    }
-
-    public int setDate(int i, int i1, int i2) {
-        return Year;
-    }
-    @Override
-    public String toString() {
-        return "Date{" +
-                "Day=" + Day +
-                ", Month=" + Month +
-                ", Year=" + Year +
-                '}';
-    }
-
-}
 
 //*
-/ The private instance variables
+//The private instance variables
    private int year, month, day;
 
    /** Constructs a Date instance with the given year, month and day. No input validation */
@@ -100,4 +53,4 @@ public class Date {
       this.month = month;
       this.day = day;
    }
-}*//
+}
